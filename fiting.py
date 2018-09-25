2b250cdf35ec80d30e2f1f09f298a626df145da8
1160300527 <15683609851@163.com>
2018-09-25 20:52:51 +0800

    最优解（有无正则）

--- fiting.py
+++ fiting.py
@@ -3,16 +3,19 @@ import numpy as np
 import math
 import os
 m = 9  # 阶数
-num = 100  # 测试数据个数
+num = 10  # 测试数据个数
+l = math.e**(-20)
 
-
-def draw(W):
-    X = np.linspace(0, 1, 100)
-    Y = np.linspace(W[0], W[0], 100)
+def draw(A,B,W):
+    x = np.linspace(0.1, 1, num)
+    X = np.linspace(1, 1, num)
     for i in range(m):
-        Y = Y + X**W[i+1]
-    print(W)
-    plt.plot(X, Y)
+        X = np.vstack([X, x**(i+1)])
+    X = X.transpose()
+    plt.plot(x, np.dot(X, W),label = "y = f(w,x)")
+    plt.plot(x,np.sin(x*np.pi*2),label="y=sin(2x)")
+    plt.scatter(A, B, color = "red", s = 10, label = "train")
+    plt.legend()
     plt.show()
 
 
@@ -24,12 +27,26 @@ def opti(X, Y):
     return np.dot(oppo, XTY)
 
 
-x = np.linspace(0, 1, num)
+def opti_reg(X,Y):
+    X_T = X.transpose()
+    XTX = np.dot(X_T, X)
+    oppo = np.linalg.inv(XTX+(np.eye(m+1))*l)
+    XTY = np.dot(X_T, Y)
+    return np.dot(oppo, XTY)
+
+
+
+def E_RMS():
+    return 0
+
+
+x = np.linspace(0.1, 1, num)
 X = np.linspace(1, 1, num)
+
 for i in range(m):
     X = np.vstack([X, x**(i+1)])
 X = X.transpose()
-print(X)
 Y = np.sin(x*2*np.pi) + np.random.normal(0, 0.1, (x.size,))  # 添加噪声的Y
-W = opti(X,Y)
-draw(W)
+W = opti_reg(X, Y)
+print(W.shape)
+draw(x,Y,W)
