bf1d8f4a99d4af3d02fd9cd6801b33d1ded4cc48
1160300527 <15683609851@163.com>
2018-09-25 15:25:29 +0800

    new

--- fiting.py
+++ fiting.py
@@ -1,21 +1,27 @@
-import matplotlib.pyplot as plt 
-import numpy as np 
+import matplotlib.pyplot as plt
+import numpy as np
 import math
 import os
-def draw():
-	x = np.linspace(0,1,66)
-	y = x + 1
-	plt.plot(x,y)
-	plt.show()
-m = 9 #阶数
-num = 10 #测试数据个数
+m = 9  # 阶数
+num = 100  # 测试数据个数
 
-x = np.linspace(0,1,num)
-X = np.linspace(1,1,num)
+
+def draw(W):
+    X = np.linspace(0, 1, 100)
+    Y = np.linspace(W[0], W[0], 100)
+    for i in range(m):
+        Y = Y + X**W[i+1]
+    print(W)
+    plt.plot(X, Y)
+    plt.show()
+
+
+x = np.linspace(0, 1, num)
+X = np.linspace(1, 1, num)
 for i in range(m):
-	X = np.vstack([X,x**(i+1
-		X = X.transpose()
-print(X.shape)
-Y = np.sin(x*2*np.pi)
-Y = np.random.normal
-W = np.linspace(0,0,m)
+    X = np.vstack([X, x**(i+1)])
+X = X.transpose()
+Y = np.sin(x*2*np.pi) + np.random.normal(0, 0.1, (x.size,))  # 添加噪声的Y
+W = np.linspace(1, 1, m+1)  # 初始化W
+
+draw(W)
