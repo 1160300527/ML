30491a0bb89df1068f7d21964fee79973fa84f2d
1160300527 <15683609851@163.com>
2018-09-28 00:05:54 +0800

    draw best fit

--- fiting.py
+++ fiting.py
@@ -3,25 +3,22 @@ import numpy as np
 import math
 import os
 m = 9  # 阶数
-num = 100  # 训练数据个数
+num = 10  # 训练数据个数
 test_num = 100 #测试数据个数
-l = math.e**(-20)
+l = math.e**(-8)
 
 
-def draw(A, B, W):
+def draw(A, B,X_test,Y_test, W,title):
     X = np.linspace(0, 1, 100)
-    X_test = np.random.random_sample(test_num,)
-    Y_test = np.sin(X_test*2*np.pi) + np.random.normal(0,0.1,test_num)
-    plt.subplot(121)
-    plt.title("Best fit without regular terms")
-   # plt.text(1,0.6,"loss = " + loss(X_test,Y_test,W), fontsize = 20)
+    plt.title(title)
+    lossing = loss(X_test,Y_test,W)
+    plt.text(-0.05,-1,"test data loss = " + str('%.5f'%lossing), fontsize = 8)
+    plt.text(-0.05,-1.2,"test data E_RMS = " + str('%.5f'%ERMS(lossing,test_num)), fontsize = 8)
     plt.plot(X, answer(X, W), label="y = f(w,x)")
     plt.plot(X, np.sin(X*np.pi*2), label="y=sin(2x)")
     plt.scatter(A, B, color="red", s=15, label="train")
     plt.scatter(X_test,Y_test, color = "green", s = 15, label = "test")
     plt.legend()
-    plt.show()
-
 
 def opti(X, Y):
     X_T = X.transpose()
@@ -40,14 +37,17 @@ def opti_reg(X, Y):
 
 
 def loss(X_test,Y_test,W):
-    X = np.linspace(1, 1, num)
+    X = np.linspace(1, 1, X_test.size)
     for i in range(m):
         X = np.vstack([X, X_test**(i+1)])
     X = X.transpose()
     Y2 = np.dot(X, W)
     L = Y_test-Y2
-    return np.dot(L.transpose(), L)
+    return np.dot(L.transpose(), L)/2
 
+
+def ERMS(loss,num):
+    return np.sqrt(2*loss/num)
 
 def answer(x, W):
     X = np.linspace(1, 1, x.size)
@@ -57,12 +57,22 @@ def answer(x, W):
     return np.dot(X, W)
 
 
+def bestFit(X,Y,X_test,Y_test):
+    W = opti(X, Y)
+    W_reg = opti_reg(X,Y)
+    plt.subplot(121)
+    draw(x, Y,X_test,Y_test, W, "Best fit without regular terms")
+    plt.subplot(122)
+    draw(x, Y,X_test, Y_test, W_reg, "Best fit with regular terms")
+    plt.show()
+
+
 x = np.linspace(0, 1, num)
 X = np.linspace(1, 1, num)
 for i in range(m):
     X = np.vstack([X, x**(i+1)])
 X = X.transpose()
 Y = np.sin(x*2*np.pi) + np.random.normal(0, 0.1, (x.size,))  # 添加噪声的Y
-W = opti_reg(X, Y)
-print(W)
-draw(x, Y, W)
+X_test = np.random.random_sample(test_num,)
+Y_test = np.sin(X_test*2*np.pi) + np.random.normal(0,0.1,test_num)
+bestFit(X,Y,X_test,Y_test)
