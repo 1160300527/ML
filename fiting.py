41278dfd4b23539e9576f66b673ca27a8af3b844
1160300527 <15683609851@163.com>
2018-10-21 12:13:06 +0800

    finally

--- fiting.py
+++ fiting.py
@@ -1,18 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-import os
 m = 9  # 阶数
 num = 100  # 训练数据个数
 test_num = 100  # 测试数据个数
 max_iters = 50000  # 梯度下级最大迭代次数
 max_value = 0.0000001  # 停止迭代条件
 step = 0.01  # 梯度下级步长
-L = -8
-l = math.e**L
+L = -8      #lambda的指数
+l = math.e**L   #lambda
 
 
-def draw(A, B, X_test, Y_test, W, title):
+def draw(A, B, X_test, Y_test, W, title):   #画函数图像
     X = np.linspace(0, 1, 100)
     plt.title(title)
     lossing = loss(X_test, Y_test, W)
@@ -23,13 +22,14 @@ def draw(A, B, X_test, Y_test, W, title):
              "\nvalidation set loss = " + str('%.5f' % lossing) +
              "\nvalidation set E_RMS = " + str('%.5f' % ERMS(lossing, test_num)), fontsize=8)
     plt.plot(X, answer(X, W), label="y = f(w,x)")
-    plt.plot(X, np.sin(X*np.pi*2), label="y=sin(2x)")
+    plt.plot(X, np.sin(X*np.pi*2), label="y=sin(2πx)")
     plt.scatter(A, B, color="red", s=15, label="train")
-    plt.scatter(X_test, Y_test, color="green", s=15, label="test")
+    plt.scatter(X_test, Y_test, color="green", s=15, label="validation")
     plt.legend()
 
 
-def bf(X, Y):
+#解析解方法求解无正则项情况
+def bf(X, Y):       
     X_T = X.transpose()
     XTX = np.dot(X_T, X)
     oppo = np.linalg.inv(XTX)
@@ -37,7 +37,8 @@ def bf(X, Y):
     return np.dot(oppo, XTY)
 
 
-def bf_reg(X, Y):
+#解析解方法求解有正则项情况
+def bf_reg(X, Y):   
     X_T = X.transpose()
     XTX = np.dot(X_T, X)
     oppo = np.linalg.inv(XTX+(np.eye(m+1))*l)
@@ -45,29 +46,32 @@ def bf_reg(X, Y):
     return np.dot(oppo, XTY)
 
 
-def gradient(X, Y):
+#梯度下降求解无正则项情况
+def gradient(X, Y): 
     W = np.linspace(0, 0, m+1)
     iter = 0
     g = gra(X, Y, W)
-    while((iter < max_iters) & (step*np.sqrt(np.dot(g.transpose(), g)) >= max_value)):
+    while((iter < max_iters) & (np.sqrt(np.dot(g.transpose(), g)) >= max_value)):
         W = W - step*g
         g = gra(X, Y, W)
         iter += 1
     return W
 
 
-def gradient_reg(X, Y):
+#梯度下降求解有正则项情况
+def gradient_reg(X, Y): 
     W = np.linspace(0, 0, m+1)
     iter = 0
     g = gra_reg(X, Y, W)
-    while((iter < max_iters) & (step*np.sqrt(np.dot(g.transpose(), g)) >= max_value)):
+    while((iter < max_iters) & (np.sqrt(np.dot(g.transpose(), g)) >= max_value)):
         W = W - step*g
         g = gra_reg(X, Y, W)
         iter += 1
     return W
 
 
-def conjugate(X, Y):
+#共轭梯度求解无正则项情况
+def conjugate(X, Y):    
     XTY = np.dot(X.transpose(), Y)
     XTX = np.dot(X.transpose(), X)
     W = np.linspace(0, 0, m+1)
@@ -85,7 +89,8 @@ def conjugate(X, Y):
     return W
 
 
-def conjugate_reg(X, Y):
+#共轭梯度求解有正则项情况
+def conjugate_reg(X, Y):    
     XTY = np.dot(X.transpose(), Y)
     XTX = np.dot(X.transpose(), X) + l*np.eye(m+1)
     W = np.linspace(0, 0, m+1)
@@ -103,17 +108,20 @@ def conjugate_reg(X, Y):
     return W
 
 
-def gra(X, Y, W):
+#根据X,Y,W求解无正则项情况下F(W)梯度
+def gra(X, Y, W):      
     XT = X.transpose()
     return np.dot(np.dot(XT, X), W)-np.dot(XT, Y)
 
 
-def gra_reg(X, Y, W):
+#根据X,Y,W求解有正则项情况下F(W)梯度
+def gra_reg(X, Y, W):   
     XT = X.transpose()
     return np.dot(np.dot(XT, X), W)-np.dot(XT, Y)+l*W
 
 
-def loss(X_test, Y_test, W):
+#损失函数，以最小二乘法计算拟合曲线的损失值
+def loss(X_test, Y_test, W):    
     X = np.linspace(1, 1, X_test.size)
     for i in range(m):
         X = np.vstack([X, X_test**(i+1)])
@@ -123,11 +131,13 @@ def loss(X_test, Y_test, W):
     return np.dot(L.transpose(), L)/2
 
 
-def ERMS(loss, num):
+#拟合优度评价
+def ERMS(loss, num):            
     return np.sqrt(2*loss/num)
 
 
-def answer(x, W):
+#计算拟合曲线上x的对应值
+def answer(x, W):               
     X = np.linspace(1, 1, x.size)
     for i in range(m):
         X = np.vstack([X, x**(i+1)])
@@ -135,7 +145,20 @@ def answer(x, W):
     return np.dot(X, W)
 
 
-def Gradient(X, Y, X_test, Y_test):
+#利用解析解进行求解并画出拟合曲线
+def BestFit(X, Y, X_test, Y_test):
+    W = bf(X, Y)
+    W_reg = bf_reg(X, Y)
+    plt.subplot(121)
+    draw(x, Y, X_test, Y_test, W, "Best fit without regular terms")
+    plt.subplot(122)
+    draw(x, Y, X_test, Y_test, W_reg, "Best fit with regular terms")
+    plt.text(-0.05, -0.6, "lambda:e^"+str(L), fontsize=8)
+    plt.show()
+
+
+#利用梯度下降法进行曲线拟合并画出结果
+def Gradient(X, Y, X_test, Y_test): #通过梯度下降法求解并画出拟合曲线
     W = gradient(X, Y)
     W_reg = gradient_reg(X, Y)
     plt.subplot(121)
@@ -149,6 +172,7 @@ def Gradient(X, Y, X_test, Y_test):
     plt.show()
 
 
+#利用共轭梯度法进行曲线拟合并画出结果
 def Conjugate(X, Y, X_test, Y_test):
     W = conjugate(X, Y)
     W_reg = conjugate_reg(X, Y)
@@ -163,25 +187,14 @@ def Conjugate(X, Y, X_test, Y_test):
     plt.show()
 
 
-def BestFit(X, Y, X_test, Y_test):
-    W = bf(X, Y)
-    W_reg = bf_reg(X, Y)
-    plt.subplot(121)
-    draw(x, Y, X_test, Y_test, W, "Best fit without regular terms")
-    plt.subplot(122)
-    draw(x, Y, X_test, Y_test, W_reg, "Best fit with regular terms")
-    plt.text(-0.05, -0.6, "lambda:e^"+str(L), fontsize=8)
-    plt.show()
-
-
-x = np.linspace(0, 1, num)
-X = np.linspace(1, 1, num)
+x = np.linspace(0, 1, num)  #生成训练集数据
+X = np.linspace(1, 1, num)  #初始化范德蒙行列式
 for i in range(m):
-    X = np.vstack([X, x**(i+1)])
-X = X.transpose()
-Y = np.sin(x*2*np.pi) + np.random.normal(0, 0.1, (x.size,))  # 添加噪声的Y
-X_test = np.random.random_sample(test_num,)
-Y_test = np.sin(X_test*2*np.pi) + np.random.normal(0, 0.1, test_num)
-BestFit(X, Y, X_test, Y_test)
-Gradient(X, Y, X_test, Y_test)
-Conjugate(X, Y, X_test, Y_test)
+    X = np.vstack([X, x**(i+1)])    
+X = X.transpose()     
+Y = np.sin(x*2*np.pi) + np.random.normal(0, 0.1, (x.size,)) # 添加噪声的Y
+X_test = np.random.random_sample(test_num,)                 #生成验证集数据 
+Y_test = np.sin(X_test*2*np.pi) + np.random.normal(0, 0.1, test_num)#计算验证集对应的Y
+BestFit(X, Y, X_test, Y_test)                               #解析解方法
+Gradient(X, Y, X_test, Y_test)                              #梯度下降法
+Conjugate(X, Y, X_test, Y_test)                             #共轭梯度法
